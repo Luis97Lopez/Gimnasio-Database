63b041687e48cef7f18f49116c84d8131c6a0a6e
Luis97Lopez <lpzr97@gmail.com>
2020-10-11 23:29:42 -0500

    Se agregan la conexion de Luis

--- Gimnasio/Gimnasio/Conexion.cs
+++ Gimnasio/Gimnasio/Conexion.cs
@@ -16,11 +16,15 @@ namespace Gimnasio
                                                 "integrated security=true;";
            
            Conexion de Luis:
+        private const string ConnectionString = "SERVER=DESKTOP-GP08O95\\SQLEXPRESS;" +
+                                    "DATABASE=Gimnasio;" +
+                                    "integrated security=true;";
 
         */
 
         // Se crea la cadena con la que se va a conectar a la base de datos.
-        private const string ConnectionString = "SERVER=MR-ICIO\\SQLEXPRESS;" +
+        //private const string ConnectionString = "SERVER=MR-ICIO\\SQLEXPRESS;" +
+        private const string ConnectionString = "SERVER=DESKTOP-GP08O95\\SQLEXPRESS;" +
                                                 "DATABASE=Gimnasio;" +
                                                 "integrated security=true;";
 
